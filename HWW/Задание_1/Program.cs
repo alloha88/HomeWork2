335abd19ae0a29fe739540f5272936715a772965
Timurgess <timurgess@yandex.ru>
2022-07-10 13:03:05 +0300

    Задание 1

---
+++ HWW/Задание_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = new Random().Next (100, 999);
+Console.WriteLine("Сгенерировано случайное число" + number);
+
+int showNumber(int number1)
+{
+    int firstStep = number1/ 10;
+    int secondStep = firstStep % 10;
+
+    return secondStep;
+}
+
+
+Console.WriteLine("Вторая цифра числа" + showNumber(number));
