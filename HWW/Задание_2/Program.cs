2eb809b6a68f3157b27623ca5376a548cbc1e87c
Timurgess <timurgess@yandex.ru>
2022-07-10 13:03:42 +0300

    Задание 2

---
+++ HWW/Задание_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу которая выводит третью цифру заданного числа или сообщает что третьей цифры нет
+
+Console.WriteLine("Введите число");
+string numbers = Console.ReadLine();
+
+string thirdNumber() {
+
+    if (numbers.Length <= 2){
+        return "В числе нет третьей цифры!";
+    }
+    else {
+        return "Третья цифра в числе " + numbers+ " "+ numbers[2];
+    }
+}
+
+
+Console.WriteLine(thirdNumber());
